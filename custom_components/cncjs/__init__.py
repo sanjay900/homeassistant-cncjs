d56c7bd809e69e4ae4963a6a83a6604b00f2a502
Sanjay Govind <sanjay.govind9@gmail.com>
2022-07-26 23:14:49 +1200

    actually connect

--- custom_components/cncjs/__init__.py
+++ custom_components/cncjs/__init__.py
@@ -6,7 +6,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 from . import controller
-from .const import CONF_USER_ID, DOMAIN, CONF_CONTROLLER_TYPE, CONF_SERIAL_PORT, CONF_BAUDRATE, ControllerType
+from .const import CONF_USER_ID, DOMAIN, CONF_CONTROLLER_TYPE, CONF_SERIAL_PORT, CONF_BAUDRATE, LOGGER, ControllerType
 from homeassistant.const import (
     CONF_IP_ADDRESS,
     CONF_ACCESS_TOKEN,
@@ -24,7 +24,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up CNCjs from a config entry."""
     # Store an instance of the "connecting" class that does the work of speaking
     # with your actual devices.
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = controller.CNCjsController(hass, entry.data[CONF_NAME], entry.data[CONF_USER_ID], entry.data[CONF_USERNAME], entry.data[CONF_ACCESS_TOKEN], entry.data[CONF_IP_ADDRESS], entry.data[CONF_PORT], entry.data[CONF_SERIAL_PORT], entry.data[CONF_BAUDRATE], ControllerType(entry.data[CONF_CONTROLLER_TYPE]))
+    cnc = controller.CNCjsController(hass, entry.data[CONF_NAME], entry.data[CONF_USER_ID], entry.data[CONF_USERNAME], entry.data[CONF_ACCESS_TOKEN], entry.data[CONF_IP_ADDRESS], entry.data[CONF_PORT], entry.data[CONF_SERIAL_PORT], entry.data[CONF_BAUDRATE], ControllerType(entry.data[CONF_CONTROLLER_TYPE]))
+    await cnc.connect()
+    if cnc.error:
+        LOGGER.error(cnc.error)
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = cnc
 
     # This creates each HA object for each platform your device requires.
     # It's done by calling the `async_setup_entry` function in each platform module.
